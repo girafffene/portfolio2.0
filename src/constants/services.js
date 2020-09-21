b2d80735c3b60ac4aca2edfa6465c5ba8b11ca0f
Irene Perez <inreallifeirene@gmail.com>
2020-09-21 13:24:23 -0700

    updated services descriptions

--- src/constants/services.js
+++ src/constants/services.js
@@ -5,12 +5,12 @@ export default [
     id: 1,
     icon: <FaCode className="service-icon" />,
     title: "web development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `I can build out the website of your dreams, whether you choose to bring me your own wireframed design or you want me to start from scratch.`,
   },
   {
     id: 2,
     icon: <FaSketch className="service-icon" />,
     title: "web design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `Need your vision to come to life? I can help wireframe a design to fit your wants.`,
   },
 ]
