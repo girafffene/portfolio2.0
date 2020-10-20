8f4edc4e77b0f9742e2ac6008d8ec66e49513db1
Irene Perez <inreallifeirene@gmail.com>
2020-10-20 15:26:43 -0700

    updated 404 content & added SEO title & desc

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,17 @@
-import React from "react"
-import Layout from "../components/Layout"
-import { Link } from "gatsby"
-// import SEO from "../components/SEO"
+//imported dependencies
+import React from "react"//React
+import { Link } from "gatsby"//Gatsby's Link
+//imported components
+import Layout from "../components/Layout"//Layout
+import SEO from "../components/SEO"//SEO
 
 const Error = () => {
   return (
     <Layout>
+      <SEO title="404" />
       <main className="error-page">
         <div className="error-container">
-          <h1>Oops, that page doesn't exist.</h1>
+          <h1>Oops, this isn't the page you are looking for.</h1>
           <Link to="/" className="btn">Home</Link>
         </div>
       </main>
