83fc557956068445ce6163c0c26e8114e683bd6e
Irene Perez <inreallifeirene@gmail.com>
2020-10-20 15:25:25 -0700

    cleaned up code

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,19 @@
-import React from "react"
+//imported dependencies
+import React from "react" //React
+//importing navigation links
 import Links from "../constants/links"
+//importing social media links
 import SocialLinks from "../constants/socialLinks"
+//importing icons
 import { FaTimes } from "react-icons/fa"
 
-const Sidebar = ({isOpen, toggleSidebar}) => {
-
+const Sidebar = ({ isOpen, toggleSidebar }) => {
   return (
     <aside className={`sidebar ${isOpen ? "show-sidebar" : ""} `} >
       <button className="close-btn" onClick={toggleSidebar}>
         <FaTimes />
       </button>
+
       <div className="side-container">
         <Links styleClass={`${isOpen ? "sidebar-links" : ""}`} />
         <SocialLinks styleClass={`${isOpen ? "sidebar-icons" : ""}`}/> 
