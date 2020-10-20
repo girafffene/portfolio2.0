de5329ba080ffccae1ae79d58771258ac9496e96
Irene Perez <inreallifeirene@gmail.com>
2020-10-20 15:27:37 -0700

    added comments & seo component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,29 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Layout from "../components/Layout"
-import Hero from "../components/Hero"
-import Services from "../components/Services"
-import Jobs from "../components/Jobs"
-import Projects from "../components/Projects"
+//imported dependencies
+import React from "react"//react
+import { graphql } from "gatsby"//graphql
+//imported components
+import Layout from "../components/Layout"//layout
+import Hero from "../components/Hero"//hero
+import Services from "../components/Services"//services
+import Jobs from "../components/Jobs"//jobs
+import Projects from "../components/Projects"//projects
+import Blogs from "../components/Blogs"//blogs
+import SEO from "../components/SEO"//seo
 
 export default ({ data }) => {
   const {
     allStrapiProjects: { nodes: projects },
-  } = data
+    allStrapiBlogs: { nodes: blogs },
+  } = data;
 
   return (
     <Layout>
+      <SEO title="Home" description="This is my home page!" />
       <Hero />
       <Services />
       <Jobs />
-      <Projects projects={projects} title="featured projects" showLink />
+      <Projects projects={projects} title="Featured Projects" showLink />
+      <Blogs blogs={blogs} title="Latest Blogs" showLink />
     </Layout>
   )
 }
@@ -27,12 +34,38 @@ export const query = graphql`
       nodes {
         github
         id
-        description
+        desc
         title
         url
+        src {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
         stack {
           id
-          title
+          name
+        }
+      }
+    }
+
+    allStrapiBlogs(sort: {fields: date, order: DESC}, limit: 3) {
+      nodes {
+        slug
+        content
+        desc
+        date(formatString: "MMM Do, YYYY")
+        id
+        title
+        category
+        src {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
         }
       }
     }
