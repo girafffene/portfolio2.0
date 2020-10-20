c93757225a604faddf5b05b43e08aafc62552d38
Irene Perez <inreallifeirene@gmail.com>
2020-10-20 15:23:56 -0700

    cleaned up jobs page

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,13 +1,15 @@
-import React from "react"
+//imported dependencies
+import React, { useState} from "react" //React & use state
+import { Link, graphql, useStaticQuery } from "gatsby" //Gatsby's Link, use static query, and graphql
+//imported title component
 import Title from "./Title"
+//imported icon
 import { FaAngleDoubleRight } from "react-icons/fa"
-import { graphql, useStaticQuery } from "gatsby"
-import { Link } from "gatsby"
-import {useState} from "react"
 
+//query to pull in Job information: company name, dates worked, position, description, and id to have a key for list items
 const query = graphql`
   {
-    allStrapiJobs(sort: {fields: id, order: DESC}) {
+    allStrapiJobs(sort: {fields: strapiId, order: DESC}) {
       nodes {
         strapiId
         company
@@ -23,18 +25,21 @@ const query = graphql`
 `
 
 const Jobs = () => {
-  const data = useStaticQuery(query)
+  //setting query equal to data to allow for destructuring
+  const data = useStaticQuery(query);
+
+  //destructuring data
   const {
     allStrapiJobs: { nodes: jobs }
-  } = data
+  } = data;
 
-  const [value, setValue] = useState(0)
-
-  const {company, position, date, desc} = jobs[value];
+  const [value, setValue] = useState(0);
+  const { company, position, date, desc } = jobs[value];
 
   return (
     <section className="section jobs">
       <Title title="experience" />
+
       <div className="jobs-center">
         <div className="btn-container">
           {jobs.map((job, i) => {
@@ -48,20 +53,22 @@ const Jobs = () => {
 
         <article className="job-info">
           <h3>{position}</h3>
+
           <h4>{company}</h4>
+
           <p className="job-date">{date}</p>
+
           {desc.map((item) => {
-              return (
-                <div key={item.id} className="job-desc">
-                  <FaAngleDoubleRight className="job-icon" /><p>{item.name}</p>
-                </div>
-              )
-            })}
+            return (
+              <div key={item.id} className="job-desc">
+                <FaAngleDoubleRight className="job-icon" /><p>{item.name}</p>
+              </div>
+            )
+          })}
         </article>
       </div>
-      <Link to="/about" className="btn center-btn">
-        more info
-      </Link>
+
+      <Link to="/about" className="btn center-btn">more info</Link>
     </section>
   )
 }
