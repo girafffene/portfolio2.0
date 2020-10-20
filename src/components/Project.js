c7be8df9b7f94ba704718dc27de32ef4405e90bd
Irene Perez <inreallifeirene@gmail.com>
2020-10-20 15:25:06 -0700

    added image to projects & updated proptypes

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,29 +1,36 @@
-import React from "react"
-import PropTypes from "prop-types"
+//imported dependencies
+import React from "react" //React
+import Image from "gatsby-image" //Gatsby's Image
+import PropTypes from "prop-types" //PropTypes
+
+//imported icons
 import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
 
-const Project = ({ description, title, github, stack, url, index }) => {
+const Project = ({ desc, title, github, stack, url, index, src }) => {
   return (
     <article className="project">
+      {src && (
+        <Image fluid={src.childImageSharp.fluid} className="project-img" />
+      )}
+
       <div className="project-info">
         <span className="project-number">0{index + 1}.</span>
-        <h3>{title}</h3>
-        <p className="project-desc">
-          {description}
-        </p>
+        <h3>{title || "This Title is Missing"}</h3>
+        <p className="project-desc">{desc}</p>
+
         <div className="project-stack">
           {stack.map((item) => {
             return (
-              <span key={item.id}>{item.title}</span>
+              <span key={item.id}>{item.name}</span>
             )
           })}
         </div>
         
         <div className="project-links">
-          <a href={github}>
+          <a href={github} rel="noopener noreferrer" target="___blank">
             <FaGithubSquare className="project-icon" />
           </a>
-          <a href={url}>
+          <a href={url} rel="noopener noreferrer" target="___blank">
             <FaShareSquare className="project-icon" />
           </a>
         </div>
@@ -34,11 +41,10 @@ const Project = ({ description, title, github, stack, url, index }) => {
 
 Project.propTypes = {
   title:PropTypes.string.isRequired,
-  github:PropTypes.string.isRequired,
-  url:PropTypes.string.isRequired,
-  description:PropTypes.string.isRequired,
-  image:PropTypes.object.isRequired,
+  desc:PropTypes.string.isRequired,
   stack:PropTypes.arrayOf(PropTypes.object).isRequired,
+  index:PropTypes.number.isRequired,
+  src:PropTypes.object.isRequired
 }
 
 export default Project
