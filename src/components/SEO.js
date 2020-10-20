2bdff4fa5117436ab2ff1483620c11a300b0b331
Irene Perez <inreallifeirene@gmail.com>
2020-10-20 15:24:49 -0700

    created seo component

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,9 +1,49 @@
-import React from "react"
-// import { Helmet } from "react-helmet"
-// import { useStaticQuery, graphql } from "gatsby"
+//imported dependencies
+import React from "react" //React
+import { Helmet } from "react-helmet"//React Helment
+import { useStaticQuery, graphql } from "gatsby" //graphql && use static query
 
-const SEO = () => {
-  return <div>SEO Component</div>
+const query = graphql`
+  {
+    site {
+      siteMetadata {
+        author
+        description
+        siteUrl
+        siteTitle: title
+        twitterUsername
+        image
+      }
+    }
+  }
+`
+
+const SEO = ({ title, description }) => {
+  const { site } = useStaticQuery(query)
+
+  const {
+    author,
+    siteDesc,
+    siteTitle,
+    siteUrl,
+    twitterUsername,
+    image
+  } = site.siteMetadata
+
+  return (
+    <Helmet title={`${title} | ${siteTitle}`} htmlAttributes={{lang: "en"}}>
+      <meta name="description" content={ description || siteDesc } />
+      <meta name="image" content={image} />
+
+      {/* twitter card */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={twitterUsername} />
+      <meta name="twitter:title" content={siteTitle} />
+      <meta name="twitter:description" content={siteDesc} />
+      <meta name="twitter:image" content={`${siteUrl}${image}`} />
+    </Helmet>
+
+  )
 }
 
 export default SEO
