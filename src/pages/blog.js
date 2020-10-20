6f010550571aff20f24a2a63b1d3488b740c8f24
Irene Perez <inreallifeirene@gmail.com>
2020-10-20 15:27:44 -0700

    created blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,42 @@
+//imported dependencies
+import React from "react"//react
+import { graphql } from "gatsby"//gatsby
+//imported components
+import Layout from "../components/Layout"//layout
+import Blogs from "../components/Blogs"//blogs
+import SEO from "../components/SEO" //SEO
+
+const Blog = ({ data: { allStrapiBlogs: { nodes: blogs }}}) => {
+  return (
+    <Layout>
+      <SEO title="Blog" description="This is my blog page. You can read all about my thoughts here lol" />
+      <section className="blog-page">
+        <Blogs blogs={blogs} title="Blog Page"/>
+      </section>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allStrapiBlogs {
+      nodes {
+        slug
+        desc
+        date(formatString: "MMM Do, YYYY")
+        id
+        title
+        category
+        src {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default Blog
