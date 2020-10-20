9c1fef38aaab38eaa9deb684f0397de4da4acc34
Irene Perez <inreallifeirene@gmail.com>
2020-10-20 15:22:48 -0700

    created a blog page

---
+++ src/components/Blog.js
@@ -0,0 +1,39 @@
+//imported dependencies
+import React from "react" //React
+import { Link } from "gatsby" //Gatsby's Link
+import Image from "gatsby-image" //Gatsby's Image
+import PropTypes from "prop-types" //PropTypes
+
+const Blog = ({ id, title, src, date, category, slug, desc }) => {
+  return (
+    <Link className="blog" to={`/blogs/${slug}`} key={id}>
+      <article>
+        {src && (
+          <Image fluid={src.childImageSharp.fluid} className="blog-img" />
+        )}
+
+        <div className="blog-card">
+          <h4>{title || "This Title is Missing"}</h4>
+          <p>{desc}</p>
+
+          <div className="blog-footer">
+            <p>{category}</p>
+            <p>{date}</p>
+          </div>
+        </div>
+      </article>
+    </Link>
+  )
+}
+
+Blog.propTypes = {
+  id:PropTypes.string.isRequired,
+  slug:PropTypes.string.isRequired,
+  title:PropTypes.string.isRequired,
+  date:PropTypes.string.isRequired,
+  category:PropTypes.string.isRequired,
+  desc:PropTypes.string.isRequired,
+  src:PropTypes.object.isRequired,
+}
+
+export default Blog
