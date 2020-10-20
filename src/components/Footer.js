d6b76cdfc185d0e9b164a6f6dd5f712b17085109
Irene Perez <inreallifeirene@gmail.com>
2020-10-20 15:23:08 -0700

    cleaned up footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,15 @@
-import React from "react"
+//imported dependencies
+import React from "react" //React
+//importing social media links
 import SocialLinks from "../constants/socialLinks"
+
 const Footer = () => {
   return (
     <footer className="footer">
       <div>
         <SocialLinks styleClass="footer-links" />
         <h4>copyright&copy;{new Date().getFullYear()}</h4>
-        <span>IPerez</span> all rights reserve
+        <span>IPerez</span> all rights reserved
       </div>
 
     </footer>
