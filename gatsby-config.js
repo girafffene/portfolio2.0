2846f114ea34e0e08b03dd24325e929a89c83c89
Irene Perez <inreallifeirene@gmail.com>
2020-10-21 14:47:31 -0700

    added cloudinary credentials && imported dotenv

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
   siteMetadata: {
     title: "IPerez",
@@ -27,6 +29,16 @@ module.exports = {
         contentTypes: [`jobs`, `projects`, `blogs`],
         singleTypes: [`about`],
       },
+    },
+    {
+      resolve: `gatsby-source-cloudinary`,
+      options: {
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        apiSecret: process.env.CLOUDINARY_API_SECRET,
+        resourceType: `image`,
+        prefix: `portfolio/` 
+      }
     },
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
