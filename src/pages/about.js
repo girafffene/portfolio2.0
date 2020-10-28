5254c0066660fce3f48279c7e8dcc8e3d89a704c
Irene Perez <inreallifeirene@gmail.com>
2020-10-28 12:07:29 -0700

    subsituted img

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,25 +1,27 @@
 //imported dependencies
 import React from "react" //React
-import { graphql } from "gatsby"//graphql
-import Image from "gatsby-image"//Gatsby's image
+import { graphql } from "gatsby" //Gatsby's graphql
+
 //imported components
 import Layout from "../components/Layout" //layout
 import Title from "../components/Title" //title component
 import SEO from "../components/SEO" //seo component
+import me from "../assets/irene-perez.jpg"
 
 const About = ({
   data: {
     about: { nodes },
   },
 }) => {
-  const{ desc, stack, title, src } = nodes[0];
+  const{ desc, stack, title } = nodes[0];
 
   return (
     <Layout>
       <SEO title="About Me" description="This page is about Irene" />
       <section className="about-page">
         <div className="section-center about-center">
-          <Image fluid={src.childImageSharp.fluid} className="about-img"/>
+          <img src={me} alt="Irene Perez" className="blog-img" />
+
           <article className="about-text">
             <Title title={title} />
             <p>{desc}</p>
@@ -47,13 +49,6 @@ export const query = graphql`
         }
         title
         desc
-        src {
-          childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
       }
     }
   }
